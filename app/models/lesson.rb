06f5696804e5470800a565a69ad4a28d36323a67
Nicolas Descreux <nicolasdescreux@hotmail.fr>
2018-12-03 14:13:20 +0100

    routes and controllers created

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,13 @@
 class Lesson < ApplicationRecord
+  has_many :participations, dependent: :destroy
+  validates :name, presence: true
+  validates :topic, presence: true
+  validates :subtopic, presence: true
+  validates :min_num_of_participants, presence: true
+  validates :max_num_of_participants, presence: true
+  validates :price, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :duration, presence: true
+  validates :description, presence: true
 end
