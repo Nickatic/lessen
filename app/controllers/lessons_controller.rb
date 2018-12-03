19295708f1565f32eb11a48e10fd879048d7d37c
Nicolas Descreux <nicolasdescreux@hotmail.fr>
2018-12-03 14:18:56 +0100

    add first methods

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,13 @@
 class LessonsController < ApplicationController
   def index
+    @lessons = Lesson.all
   end
 
   def show
+    @lesson = Lesson.find(params[:id])
   end
 
   def new
+    @lesson = Lesson.new
   end
 end
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,2 +1,13 @@
 class ParticipationsController < ApplicationController
+  def create
+    @participation = Participation.new
+    @lesson = Lesson.find(params[:lesson_id])
+    @participation.lesson = @lesson
+    @participation.user = current_user
+    if @participation.save
+      redirect_to lesson_path(@lesson)
+    else
+      render "lessons/show"
+    end
+  end
 end
