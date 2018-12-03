d8ed1f286d9d643058145c42de5d30a071645202
Heloise Guillemot <heloise.guillemot@gmail.com>
2018-12-03 15:57:06 +0100

    content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_120643) do
+ActiveRecord::Schema.define(version: 2018_12_03_145355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2018_12_03_120643) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_lessons_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content"
   end
 
   create_table "participations", force: :cascade do |t|
