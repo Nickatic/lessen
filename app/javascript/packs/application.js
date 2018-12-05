331f2b035d520875cdb6c970ced8d58d2681c00c
Anna Hercot <anna.hercot96@gmail.com>
2018-12-05 18:27:51 +0100

    chatjavaetc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,17 +2,21 @@ import "bootstrap";
 
 import flatpickr from "flatpickr";
 import { initFlatpickr } from "../plugins/flatpickr"
-import { initChannel } from "../plugins/action_cable"
+import { initChannel, scrollLastMessageIntoView } from "../plugins/action_cable"
 
 
 initFlatpickr()
 
 import { initUpdateNavbarOnScroll } from '../components/navbar'
+import { submitChatForm } from '../components/submitChatForm'
+
 
 
 
 initUpdateNavbarOnScroll();
 initChannel();
+scrollLastMessageIntoView();
+submitChatForm();
 
 
 
