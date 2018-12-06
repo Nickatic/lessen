a63858a15935a7f9e7f650ef62f0bc9ba3dfdf87
Nicolas Descreux <nicolasdescreux@hotmail.fr>
2018-12-06 11:23:37 +0100

    indent properly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,19 @@
 class ReviewsController < ApplicationController
   def create
-  @lesson = Lesson.find(params[:lesson_id])
-  @review = Review.new(review_params)
-  @review.lesson = @lesson
-  if @review.save
-    respond_to do |format|
-      format.html { redirect_to lesson_path(@lesson) }
-      format.js  # <-- will render `app/views/reviews/create.js.erb`
+    @lesson = Lesson.find(params[:lesson_id])
+    @review = Review.new(review_params)
+    @review.lesson = @lesson
+    if @review.save
+      respond_to do |format|
+        format.html { redirect_to lesson_path(@lesson) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'lessons/index' }
+        format.js  # <-- idem
+      end
     end
-  else
-    respond_to do |format|
-      format.html { render 'lessons/index' }
-      format.js  # <-- idem
-    end
-  end
   end
 
   private
