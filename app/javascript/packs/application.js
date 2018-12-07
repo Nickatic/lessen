5e28766390acedda5e7940457f44196e9a52034b
Lily Gasztowtt <lilyjoannag@gmail.com>
2018-12-07 14:43:45 +0100

    changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,27 @@
 import "bootstrap";
+import "slick-carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import flatpickr from "flatpickr";
-import { initFlatpickr } from "../plugins/flatpickr"
-import { initChannel, scrollLastMessageIntoView } from "../plugins/action_cable"
+import { initFlatpickr } from "../plugins/flatpickr";
+import { initChannel, scrollLastMessageIntoView } from "../plugins/action_cable";
 
 
-initFlatpickr()
+initFlatpickr();
 
-import { initUpdateNavbarOnScroll } from '../components/navbar'
-import { submitChatForm } from '../components/submitChatForm'
-import { initFormRefresh } from '../components/sort_by'
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { submitChatForm } from '../components/submitChatForm';
+import { initFormRefresh } from '../components/sort_by';
 
 
 
-initFormRefresh()
+initFormRefresh();
 initUpdateNavbarOnScroll();
 initChannel();
 scrollLastMessageIntoView();
 submitChatForm();
 
+import { carouselIndex } from '../components/carousel-participations-index.js';
 
+carouselIndex();
