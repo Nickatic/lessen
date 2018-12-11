b93ca2495011b26e3286d806ca558c3fa7614282
Heloise Guillemot <heloise.guillemot@gmail.com>
2018-12-11 13:13:21 +0100

    show

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ApplicationRecord
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable_on :skills, :objectives
   has_many :participations, dependent: :destroy
   has_many :messages, dependent: :destroy
   belongs_to :teacher, class_name: "User", foreign_key: :user_id
@@ -41,8 +43,6 @@ class Lesson < ApplicationRecord
   validates :duration, presence: true
   validates :description, presence: true
   validates :summary, presence: true
-  validates :objectives, presence: true
-  validates :skills, presence: true
 
 
   def update_price_per_user
