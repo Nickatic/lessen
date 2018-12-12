c3dd540c9c6979c999ac7a3e531e84bab6ca3fe7
Anna Hercot <anna.hercot96@gmail.com>
2018-12-12 12:36:43 +0100

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_095648) do
+ActiveRecord::Schema.define(version: 2018_12_12_113056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_095648) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
-    t.integer "rating"
+    t.float "rating"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
